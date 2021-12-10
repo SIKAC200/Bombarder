e0ef5ee0b36114174f01e1c4c16eb0401e3b9607
SIKAC200 <95934594+SIKAC200@users.noreply.github.com>
2021-12-10 21:30:22 +0100

    Update BombardEmail.php

--- app/Mail/BombardEmail.php
+++ app/Mail/BombardEmail.php
@@ -28,6 +28,6 @@ class BombardEmail extends Mailable
      */
     public function build()
     {
-        return $this->view('emails.bombarderMail');
+        return $this->view('emails.mail');
     }
 }
